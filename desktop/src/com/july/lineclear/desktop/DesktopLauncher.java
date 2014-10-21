bb63f7798a88d915e082f5ceb016832cc780814e
wzb <bangzewu@126.com>
2014-10-21 15:21:53 +0800

    initial source code

---
+++ desktop/src/com/july/lineclear/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.july.lineclear.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.july.lineclear.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
